649b78e7663c5267713c026fcf0e32bd4233328a
Petri Lehtinen <petri@digip.org>
2009-09-04 21:19:45 +0300

    Remove everything related to news

--- models.py
+++ models.py
@@ -1,18 +1,6 @@
-import operator
 import os
 import simplejson
 from util import rst
-
-class NewsItem(object):
-    def __init__(self, project, date, title, text):
-        self.project = project
-        self.date = date
-        self.title = title
-        self.text = text
-
-    def __lt__(self, other):
-        return self.date < other.date
-
 
 class Project(object):
     def __init__(self, slug):
@@ -27,18 +15,6 @@ class Project(object):
         with open(os.path.join(path, 'index.html')) as fobj:
             self.index = fobj.read()
 
-        if os.path.isdir(os.path.join(path, 'news')):
-            def _generate():
-                for filename in os.listdir(os.path.join(path, 'news')):
-                    date = os.path.splitext(filename)[0]
-                    title, text = rst(os.path.join(path, 'news', filename))
-                    yield NewsItem(self, date, title, text)
-            self.all_news = sorted(_generate(), reverse=True)
-        else:
-            self.all_news = []
-
-        self.news = self.all_news[:5]
-
     def __lt__(self, other):
         return self.name < other.name
 
@@ -50,8 +26,5 @@ for dir in next(os.walk('pagedata/projects'))[1]:
 class Index(object):
     def __init__(self, projects):
         self.projects = sorted(projects.values())
-        self.all_news = reduce(operator.add, (x.news for x in self.projects))
-        self.all_news.sort(reverse=True)
-        self.news = self.all_news[:5]
 
 index = Index(projects)
