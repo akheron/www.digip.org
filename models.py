0799728a473ee9f0e416f2cd6b01cffc6f55f6b5
Petri Lehtinen <petri@digip.org>
2009-10-21 21:30:11 +0300

    The first try for Python 2.4 compatibility

--- models.py
+++ models.py
@@ -1,24 +1,34 @@
 import os
-import simplejson
+try:
+    import json
+except ImportError:
+    import simplejson as json
 
 class Project(object):
     def __init__(self, slug):
         path = os.path.join('pagedata/projects', slug)
-        with open(os.path.join(path, 'project.json')) as fobj:
-            metadata = simplejson.load(fobj)
+
+        fobj = open(os.path.join(path, 'project.json'))
+        try:
+            metadata = json.load(fobj)
+        finally:
+            fobj.close()
 
         self.slug = slug
         self.name = metadata['name']
         self.description = metadata['description']
 
-        with open(os.path.join(path, 'index.html')) as fobj:
+        fobj = open(os.path.join(path, 'index.html'))
+        try:
             self.index = fobj.read()
+        finally:
+            fobj.close()
 
     def __lt__(self, other):
         return self.name < other.name
 
 projects = {}
-for dir in next(os.walk('pagedata/projects'))[1]:
+for dir in os.walk('pagedata/projects').next()[1]:
     projects[dir] = Project(os.path.basename(dir))
 
 
