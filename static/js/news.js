34565521ff9523d3efecbf33644dd8f757582d18
Petri Lehtinen <petri@digip.org>
2009-10-28 23:13:26 +0200

    Make the news script ultimate

--- static/js/news.js
+++ static/js/news.js
@@ -1,18 +1,44 @@
-var amount = 5;
+var step = 5;
 
-function hide_news() {
-    $('#news tr:gt(' + (amount - 1) + ')').hide();
-    if($('#news tr:hidden').length) {
-        $('#news').after('<p><a id="morenews" href="javascript:;">(Show more news)</a></p>');
-        $('#morenews').click(
-            function() {
-                $('#news tr:hidden:lt(' + amount + ')').show();
-                if(!$('#news tr:hidden').length) {
-                    $('#morenews').hide();
-                }
-            }
-        );
+function show_news(count) {
+    // Make count a multiple of step
+    count = count + ((step - (count % step)) % step);
+
+    $('#news tr:lt(' + count + ')').show();
+    $('#news tr:gt(' + (count - 1) + ')').hide();
+
+    if($('#news tr:hidden').length == 0) {
+        $('#morenews').hide();
+    } else {
+        $('#morenews').show();
+    }
+
+    if($('#news tr:visible').length == step) {
+        $('#lessnews').hide();
+    } else {
+        $('#lessnews').show();
     }
 }
 
-$(document).ready(hide_news);
+function init_news() {
+    $('#news').after(
+            '<p>' +
+            '<a id="morenews" href="javascript:;">(Show more news)</a> ' +
+            '<a id="lessnews" href="javascript:;">(Show less news)</a>' +
+            '</p>');
+    $('#morenews').click(
+        function() {
+            show_news($('#news tr:visible').length + step);
+        });
+    $('#lessnews').click(
+        function() {
+            show_news($('#news tr:visible').length - step);
+        });
+}
+
+$(document).ready(
+    function() {
+        init_news();
+        show_news(step);
+    }
+);
